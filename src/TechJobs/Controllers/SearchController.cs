404bab30eece2b362a5d55ab40b44e5b817f6ad5
ramya4792 <56323278+ramya4792@users.noreply.github.com>
2020-03-10 15:58:17 -0500

    Add files via upload

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,20 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+
+            List<Dictionary<string, string>> results = new List<Dictionary<string, string>>();
+            if (searchType == "all")
+                results = JobData.FindByValue(searchTerm);
+            else
+                results = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.jobs = results;
+            return View("Index");
+            
+        }
 
     }
 }
